e22dad93fb9275dae656af66fef0170b5d78b8ba
jeremy550 <s3804383@student.rmit.edu.au>
2020-08-22 23:54:56 +1000

    Revert "UI of login and registration"
    This reverts commit bd7d78a34b37296c9249b3fee45cb3f34570029a.

--- FrontEnd/myapp/src/App.js
+++ FrontEnd/myapp/src/App.js
@@ -1,26 +1,26 @@
-import React, {Component} from 'react';
-import Login from './components/Login';
-import Registration from './components/Registration';
-import NavBar from './components/NavBar';
-import{BrowserRouter,Route,Switch} from 'react-router-dom'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render () {
-     return (
-      <BrowserRouter>
-      <NavBar/>
-
-      <div className="App">    
-             
-      <Switch>
-       <Route path='/' component={Login} exact/>
-       <Route path='/Registration' component={Registration}/>
-      </Switch>
-      </div>
-      </BrowserRouter>
-     )
-   }
- 
- }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
