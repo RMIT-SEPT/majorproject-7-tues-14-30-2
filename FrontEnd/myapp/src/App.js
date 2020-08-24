ebd3b8fe1d7b5045bafd0649aaa58a3f3f7e4a86
shreyasamanta <shreysaman@gmail.com>
2020-08-24 21:03:50 +1000

    Admin Dashboard

--- FrontEnd/myapp/src/App.js
+++ FrontEnd/myapp/src/App.js
@@ -4,6 +4,7 @@ import Registration from './components/Registration';
 import Business_Contact from './components/Business_Contact';
 import Worker_Dashboard from './components/Worker_Dashboard';
 import Customer_Dashboard from './components/Customer_Dashboard';
+import Admin_Dashboard from './components/Admin_Dashboard';
 import Booking from './components/Booking';
 import NavBar from './components/NavBar';
 import{BrowserRouter,Route,Switch} from 'react-router-dom'
@@ -20,6 +21,7 @@ class App extends Component {
       <Route path='/Business_Contact' component={Business_Contact}/>
       <Route path='/Registration' component={Registration}/>
       <Route path='/Customer_Dashboard' component={Customer_Dashboard}/>
+      <Route path='/Admin_Dashboard' component={Admin_Dashboard}/>
       <Route path='/Worker_Dashboard' component={Worker_Dashboard}/>
       <Route path='/Booking' component={Booking}/>
        <Route path='/' component={Login} exact/>
