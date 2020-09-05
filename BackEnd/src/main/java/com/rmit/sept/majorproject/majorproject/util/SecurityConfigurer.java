a383afb65c15108c844346dd933ae9a7bfebe2b6
Anthony Nguyen s3783719 <s3783719@student.rmit.edu.au>
2020-09-06 09:56:32 +1000

    Added h2-console url exception and booking tables

--- BackEnd/src/main/java/com/rmit/sept/majorproject/majorproject/util/SecurityConfigurer.java
+++ BackEnd/src/main/java/com/rmit/sept/majorproject/majorproject/util/SecurityConfigurer.java
@@ -31,7 +31,8 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll()
-                .antMatchers("/api/*").permitAll().
+                .antMatchers("/api/*").permitAll()
+                .antMatchers("/h2-console/*").permitAll().
                 anyRequest().authenticated().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
