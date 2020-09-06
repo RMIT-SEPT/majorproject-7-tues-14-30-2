c078ac4f485211eb8549b5380fbbed966f4e03a6
Anthony Nguyen s3783719 <s3783719@student.rmit.edu.au>
2020-09-06 20:05:54 +1000

    fixed h2-console exception

--- BackEnd/src/main/java/com/rmit/sept/majorproject/majorproject/util/SecurityConfigurer.java
+++ BackEnd/src/main/java/com/rmit/sept/majorproject/majorproject/util/SecurityConfigurer.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -32,10 +33,20 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll()
                 .antMatchers("/api/*").permitAll()
-                .antMatchers("/h2-console/*").permitAll().
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/h2-console").permitAll().
                 anyRequest().authenticated().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        http.csrf().disable();
+//        http.headers().frameOptions().disable();
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
     }
 
     @Override
--- BackEnd/src/main/java/com/rmit/sept/majorproject/majorproject/web/BookingController.java
+++ BackEnd/src/main/java/com/rmit/sept/majorproject/majorproject/web/BookingController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/booking")
@@ -20,8 +24,11 @@ public class BookingController {
 
     @PostMapping("")
     public ResponseEntity<?> createNewBooking(@Valid @RequestBody Booking booking, BindingResult result){
-        if(result.hasErrors()){
-            return new ResponseEntity<String>("Invalid User Object", HttpStatus.BAD_REQUEST);
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error : result.getFieldErrors()) {
+                return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
+            }
         }
         Booking booking1 = bookingService.saveOrUpdateBooking(booking);
         return new ResponseEntity<Booking>(booking, HttpStatus.CREATED);
