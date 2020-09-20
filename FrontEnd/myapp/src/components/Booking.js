4bdecffd0d8f854df82bdade93dbc7e375ef1093
jeremy550 <s3804383@student.rmit.edu.au>
2020-09-21 00:49:54 +1000

    connection between front and back

--- FrontEnd/myapp/src/components/Booking.js
+++ FrontEnd/myapp/src/components/Booking.js
@@ -1,39 +1,168 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import"./Booking.css";
-import DateField from './DateField'
-import TimeField from './TimeField'
+import axios from 'axios';
+import DatePicker from "react-datepicker";
+import moment from 'moment';
+import TimePicker from 'rc-time-picker';
+import "react-datepicker/dist/react-datepicker.css";
+import 'rc-time-picker/assets/index.css';
+
+
 
 
 class Booking extends Component{
     constructor(props){
         super(props)
     
-    
     this.state={
-            date:'',
-            time:''
-
+            date: new Date(),
+            time:'',
+            worker:'',
+            service:'',
+            duration:'',
+            notes:'',
+    };
     }
-   
-    }
+    
 
-confirmMessage() {
-    alert('Your booking has been confirmed!');
+handleNotesChange = (event) =>{
+    this.setState({
+        notes: event.target.value 
+    })
 }
 
+handleServiceChange = (event) =>{
+    this.setState({
+        service: event.target.value 
+    })
+}
+
+handleWorkerChange = (event) =>{
+    this.setState({
+        worker: event.target.value 
+    })
+}
+
+handleDurationChange = (event) =>{
+    this.setState({
+        duration: event.target.value,
+    })
+}
+
+handleDateChange = (date) => {
+    this.setState({
+        //convert to year-month-day
+      date :moment(date).format('YYYY-MM-DD')
+
+      })
+  }
+
+handleTimeChange = (time) => {
+    this.setState({
+      time:time
+    })
+  }
+
+onSubmit = (event) =>{
+    event.preventDefault() 
+    var info={
+        workername:this.state.worker,
+        servicename:this.state.service
+    }
+    var workername = info.workername;
+    var servicename = info.servicename;
+    
+    var username = localStorage.getItem("username");
+    var password = localStorage.getItem("user_password");
+    var name = localStorage.getItem("user_name");
+    var address = localStorage.getItem("user_address");
+    var contact = localStorage.getItem("user_contact");
+    var role = localStorage.getItem("user_role");
+    //convert timestamp to hh-mm-ss
+    var time =moment(this.state.time).format('HH:mm:ss');
+    axios.post(`http://localhost:8080/api/booking/${workername}/${servicename}`,{
+        booking_date: this.state.date,
+        booking_time:time,
+        notes: this.state.notes,
+        customer : 
+            {
+                username:username,
+                password: password,
+                name: name,
+                address: address,
+                contact: contact,
+                role: role
+            },
+        booked_service: "Service1",
+        duration: this.state.duration
+    })
+
+        .then((res)=>{
+            console.log(res)
+                alert('Your booking has been confirmed!');
+
+        })
+        
+        .catch((error)=>{
+            console.log(error)
+        })
+
+    
+}
+
+
 render(){
-    const {username,password} = this.state
+
 return(
     <div> 
-        
-        <h1 class='title'>Book an Appointment</h1>
+        <h1 className='title'>Book an Appointment</h1>
         <br></br>
-        <div class = "inputField">
-            <label>Date</label> <DateField/>
+
+        <form onSubmit={this.onSubmit}>
+        <div className = "inputField">
+            <label>Service</label> 
             <br/>
-            <label>Time</label> <TimeField/>
+            <select name = 'service' value={this.state.service} onChange={this.handleServiceChange}> 
+                  <option name="service1">Service1</option>
+                  <option name="service2">Service2</option>
+                  <option name="service3">Service3</option>
+            </select>
+            <br/>
+            <br/>
+            <label>Worker</label> 
+            <br/>
+            <select name = 'worker' value={this.state.worker} onChange={this.handleWorkerChange}> 
+                  <option name="worker1">Worker1</option>
+                  <option name="worker2">Worker2</option>
+                  <option name="worker3">Worker3</option>
+            </select>
+            <br/>
+            <br/>
+            <label>Date</label> 
+            <br/>
+            <DatePicker value={this.state.date} onChange={this.handleDateChange} minDate={new Date()}/>
+            <br/>
+            <label>Time</label> 
+            <br/>
+            <TimePicker value={this.state.time} onChange={this.handleTimeChange} />
+
+              <br/>
+            <br/>
+            <label>Duration(mins)</label>
+            <br/>
+            <input type='number' min="1" name='duration' value={this.state.duration} onChange={this.handleDurationChange}/>
+            <br/>
+           
+            <label>Notes</label>
+            <br/>
+            <input type = 'text' name = 'notes' value={this.state.notes} onChange={this.handleNotesChange}/>
+            <br/>
+            
+       
         </div>
-        <button className="book_btn" onClick={this.confirmMessage}>Book</button>   
+        <button type = 'submit' className="book_btn" onClick={this.onSubmit}>Book</button>   
+
+        </form>
 
     </div>
      )       
