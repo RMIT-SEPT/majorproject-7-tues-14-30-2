35f39a81602527c2524d2e335fc03dc6784fa2ba
Catherine Thomas <s3784710@student.rmit.edu.au>
2020-10-06 00:50:45 +1100

    fixed merge conflicts

--- FrontEnd/myapp/src/components/Booking.js
+++ FrontEnd/myapp/src/components/Booking.js
@@ -100,6 +100,21 @@ handleTimeChange = (time) => {
     var role = localStorage.getItem("user_role");
     //convert timestamp to hh-mm-ss
     var time =moment(this.state.time).format('HH:mm:ss');
+    
+    
+    if(this.state.date == null) {
+        alert('Please select a date');
+    } 
+    else if(this.state.time == null || this.state.time == '') {
+        alert('Please select a time');
+    }
+    else if(this.state.duration == null || this.state.duration == '') {
+        alert('Please fill in the duration');
+    }
+    else if(this.state.notes == null || this.state.notes == '') {
+        alert('Please fill in the notes');
+    }
+    else {
     axios.post(`http://localhost:8080/api/booking/${workername}/${servicename}`,{
         booking_date: this.state.date,
         booking_time:time,
@@ -116,41 +131,27 @@ handleTimeChange = (time) => {
         booked_service:servicename,
         duration: this.state.duration
     })
-
         .then((res)=>{
             console.log(res)
                 alert('Your booking has been confirmed!');
-
+                window.location.reload(false);
         })
         
+
         .catch((error)=>{
             console.log(error)
         })
 
-        if(this.state.date == null) {
-            alert('Please select a date');
-        } 
-        else if(this.state.time == null || this.state.time == '') {
-            alert('Please select a time');
-        }
-        else if(this.state.duration == null || this.state.duration == '') {
-            alert('Please fill in the duration');
-        }
-        else if(this.state.notes == null || this.state.notes == '') {
-            alert('Please fill in the notes');
-        }
-        else {
-            alert('Your booking has been confirmed!');
-            window.location.reload(false)
-        }      
+    }
+          
 }
-
 
 render(){
 return(
     <div> 
         <h1 class='title'>Book an Appointment</h1>
         <br></br>
+
         <h2 id='heading'>Available Services</h2>
               <table id='services'>
                  <thead>
@@ -176,10 +177,9 @@ return(
                           </tr>
                       ))
                   }   
-                    <tr></tr>
-                    
                  </tbody>
               </table>
+              
          <form onSubmit={this.onSubmit}>
         <div class = "inputField">
             <label>Service</label> 
