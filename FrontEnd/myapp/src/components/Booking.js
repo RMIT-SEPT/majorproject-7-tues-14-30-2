0348eb6e39f201949fabe0d22d7bfd96a048706c
Catherine Thomas <s3784710@student.rmit.edu.au>
2020-10-06 12:59:56 +1100

    added validation so booking cannot be made if no available services

--- FrontEnd/myapp/src/components/Booking.js
+++ FrontEnd/myapp/src/components/Booking.js
@@ -102,18 +102,29 @@ handleTimeChange = (time) => {
     var time =moment(this.state.time).format('HH:mm:ss');
     
     
-    if(this.state.date == null) {
+    if(this.state.services.length === 0) {
+        alert('Sorry no available services!');
+    }
+    else if(this.state.service === null || this.state.service === '' || this.state.service === 'default') {
+        alert('Please select a service');
+    }
+    else if(this.state.worker === null || this.state.worker === '' || this.state.worker === 'default') {
+        alert('Please select a worker');
+    }
+    else if(this.state.date === null) {
         alert('Please select a date');
     } 
-    else if(this.state.time == null || this.state.time == '') {
+    else if(this.state.time === null || this.state.time === '') {
         alert('Please select a time');
     }
-    else if(this.state.duration == null || this.state.duration == '') {
+    else if(this.state.duration === null || this.state.duration === '') {
         alert('Please fill in the duration');
     }
-    else if(this.state.notes == null || this.state.notes == '') {
+    else if(this.state.notes === null || this.state.notes === '') {
         alert('Please fill in the notes');
     }
+    
+    
     else {
     axios.post(`http://localhost:8080/api/booking/${workername}/${servicename}`,{
         booking_date: this.state.date,
@@ -158,7 +169,7 @@ return(
                      <th>ID</th>
                      <th>Service</th>
                      <th>Worker</th>
-                     <th>Date</th>
+                     <th>Days</th>
                      <th>Time</th>
                  </thead> 
                  <tbody>
@@ -185,19 +196,21 @@ return(
             <label>Service</label> 
             <br/>
             <select name = 'service' value={this.state.service} onChange={this.handleServiceChange}> 
-                  <option name="service1">Service1</option>
-                  <option name="service2">Hair Salon</option>
-                  <option name="service3">Accounting Firm</option>
+                    <option name="default" value="default">-- Select a service --</option>
+                    <option name="service1">Service 1</option>
+                    <option name="service2">Service 2</option>
+                    <option name="service3">Service 3</option>
             </select>
             <br/>
             <br/>
             <label>Worker</label> 
             <br/>
             <select name = 'worker' value={this.state.worker} onChange={this.handleWorkerChange}> 
-                  <option name="worker1">Worker1</option>
-                  <option name="worker2">Stephanie</option>
-                  <option name="worker3">Catherine</option>
-                  </select>
+                  <option name="default" value="default">-- Select a worker --</option>
+                  <option name="worker1">Worker 1</option>
+                  <option name="worker2">Worker 2</option>
+                  <option name="worker3">Worker 3</option>
+            </select>
             <br/>
             <br/>
             <label>Date</label> 
